54c8e5bf03cdc2a1c2595b9b27f1a25d3408fdf4
Gabriel Dias <gabrieldias2032@gmail.com>
2021-07-24 21:54:01 -0300

    Player Interact

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField, Range(0f, 10f)] float speed;
     [SerializeField] Animator anim;
+    GameObject objeto;
 
     Vector2 direction;
 
@@ -27,5 +28,31 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = direction;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Iinteractable>() != null)
+        {
+            objeto = other.GetComponent<GameObject>();
+            StartCoroutine(Interact(objeto));
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Iinteractable>() != null)
+        {
+            StartCoroutine("Interact");
+        }
+    }
+    private IEnumerator Interact(GameObject objeto)
+    {
+        while(true)
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                objeto.GetComponent<CanhaoBehaviour>().Interact();
+            }
+            yield return null;
+        }
     }
 }
