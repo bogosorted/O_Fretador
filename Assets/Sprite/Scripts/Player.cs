df6dbc92f5abc476ce50f0fc3b1d75a4b0edca4b
Giribroba <Giribroba@gmail.com>
2021-07-24 21:05:57 -0300

    Personagem anda

---
+++ Assets/Sprite/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField, Range(0f, 10f)] float speed;
+    [SerializeField] Animator anim;
+
+    Vector2 direction;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed).normalized;
+
+        anim.SetBool("Andando",rb.velocity.magnitude != 0);
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            this.transform.up = direction;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = direction;
+    }
+}
