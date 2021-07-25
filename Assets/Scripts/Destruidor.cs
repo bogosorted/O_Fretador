fe80a89bec1d81b0e0b9491ab107741370458d0b
Giribroba <Giribroba@gmail.com>
2021-07-25 01:00:06 -0300

    infinito

---
+++ Assets/Scripts/Destruidor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruidor : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Navio>() != null)
+        {
+            Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WatherRouteGen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatherRouteGen : MonoBehaviour
+{
+    [SerializeField] GameObject next, barco;
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Navio>() != null)
+        {
+            var correct = Vector3.Dot(this.transform.up, barco.transform.up) > 0 ? 1 : -1;
+            Instantiate(next, (this.transform.position.y + sr.size.y * correct) * this.transform.up , this.transform.rotation);
+        }
+    }
+}
