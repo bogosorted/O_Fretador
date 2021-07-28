85a41feec493c74335c7be190efde26d5195ef93
gustavosalm <gustavosalmeida9821@gmail.com>
2021-07-28 01:22:12 -0300

    codigo inicial das ondas

---
+++ Assets/Scripts/WaterBehaviour/Floater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    [SerializeField]private Rigidbody rb;
+    [SerializeField]private float depthBeforeSubmerged = 1f, displacementAmount = 3f, waterDrag = 2f, waterAngularDrag = 2f;
+    [SerializeField, Min(1)]private int floaterCount = 1;
+
+    private void FixedUpdate() {
+        rb.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeigth = WaveManager.instance.GetWaveHeight(transform.position.x);
+        if(transform.position.y < waveHeigth){
+            float displacementMultipler = Mathf.Clamp01((waveHeigth - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementAmount, 0f), transform.position, ForceMode.Acceleration);
+            rb.AddForce(displacementMultipler * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacementMultipler * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Assets/Scripts/WaterBehaviour/WaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class WaveManager : MonoBehaviour
+{
+    static public WaveManager instance;
+    private MeshFilter mf;
+    [SerializeField]private float amplitude = 1f, length = 2f, speed = 1f, offset = 0f;
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        mf = GetComponent<MeshFilter>();
+    }
+
+    void Update()
+    {
+        offset += speed * Time.deltaTime;
+        Vector3[] vertices = mf.mesh.vertices;
+        for(int i = 0; i < vertices.Length; i++){
+            vertices[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x);
+        }
+        mf.mesh.vertices = vertices;
+        mf.mesh.RecalculateNormals();
+    }
+
+    public float GetWaveHeight(float x){
+        return amplitude * Mathf.Sin(x / length + offset);
+    }
+}
