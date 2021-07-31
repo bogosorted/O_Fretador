5d4f55b3aa3a5dc7e8bfb346442c527fa194703d
Gabriel Aragão <gabrielaragaofds@gmail.com>
2021-07-31 20:03:25 -0300

    degree caravel limitation

--- Assets/Scripts/Navio.cs
+++ Assets/Scripts/Navio.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Navio : MonoBehaviour
 {
     [SerializeField, Range(0f, 10f)] public float speed;
-    [SerializeField, Range(0f, 8f)] private int life;
+   // [SerializeField, Range(0f, 8f)] private int life;
     public bool ancorado;
     public Player pl;
     public GameObject lifeBarFill;
@@ -14,9 +14,9 @@ public class Navio : MonoBehaviour
     private List<int> repairsLeft = new List<int>() {6, 4, 2};
 
     void Start(){
-        if(pl == null)
-            pl = transform.GetChild(0).GetComponent<Player>();
-        lifeBarFullSize = lifeBarFill.GetComponent<RectTransform>().sizeDelta;
+     //   if(pl == null)
+      //      pl = transform.GetChild(0).GetComponent<Player>();
+     //lifeBarFullSize = lifeBarFill.GetComponent<RectTransform>().sizeDelta;
     }
 
     void FixedUpdate()
@@ -29,31 +29,31 @@ public class Navio : MonoBehaviour
             TakeDamage(1);
         }
         if(Input.GetKeyDown(KeyCode.G)){
-            StartCoroutine("RepairBoat");
+          //  StartCoroutine("RepairBoat");
         }
     }
 
     public void TakeDamage(int val){
-        life -= val;
-        if(life == 0)
-            print("u ded");
-        else {
-            if(repairsLeft.Contains((life % 2 == 0) ? life : life + 1)){
-                print("cria reparo");
-                repairsLeft.Remove((life % 2 == 0) ? life : life + 1);
-            }
-            print(life);
-        }
-        lifeBarFill.GetComponent<RectTransform>().sizeDelta = new Vector3(lifeBarFullSize.x * ((float)life / 8), lifeBarFullSize.y);
-        print((8 / (float)life));
+        // life -= val;
+        // if(life == 0)
+        //     print("u ded");
+        // else {
+        //     if(repairsLeft.Contains((life % 2 == 0) ? life : life + 1)){
+        //         print("cria reparo");
+        //         repairsLeft.Remove((life % 2 == 0) ? life : life + 1);
+        //     }
+        //     print(life);
+        // }
+       // lifeBarFill.GetComponent<RectTransform>().sizeDelta = new Vector3(lifeBarFullSize.x * ((float)life / 8), lifeBarFullSize.y);
+        //print((8 / (float)life));
     }
 
-    public IEnumerator RepairBoat(){
-        pl.movable = false;
-        yield return new WaitForSeconds(1);
-        pl.movable = true;
-        life++;
-        lifeBarFill.GetComponent<RectTransform>().sizeDelta = new Vector3(lifeBarFullSize.x * ((float)life / 8), lifeBarFullSize.y);
-        print(life);
-    }
+    //public IEnumerator RepairBoat(){
+        // pl.movable = false;
+        // yield return new WaitForSeconds(1);
+        // pl.movable = true;
+        // life++;
+        // lifeBarFill.GetComponent<RectTransform>().sizeDelta = new Vector3(lifeBarFullSize.x * ((float)life / 8), lifeBarFullSize.y);
+        // print(life);
+   // }
 }
--- Assets/Scripts/Obj Interagiveis/TimaoBehaviour.cs
+++ Assets/Scripts/Obj Interagiveis/TimaoBehaviour.cs
@@ -8,22 +8,37 @@ public class TimaoBehaviour : MonoBehaviour, Iinteractable
     GameObject goNavio;
     Navio navio;
     float lastState;
+
+    Quaternion lastAngle;
+    float deltaAngleX,actualAngulation;
     void Awake()
     {
         lastState = 0;
+        lastAngle = Quaternion.identity;
         goNavio = this.transform.parent.gameObject;
         navio = goNavio.GetComponent<Navio>();
     }
     private void FixedUpdate()
     {
-
+        
         if (controlando)
             lastState = Mathf.Clamp(lastState + -Input.GetAxisRaw("Horizontal") / 30, -1, 1);
         if (!navio.ancorado)
         {
-            goNavio.transform.parent.rotation = Quaternion.Euler(Vector3.forward * lastState + goNavio.transform.parent.rotation.eulerAngles);
-            goNavio.transform.localRotation = Quaternion.Euler(new Vector3(0, lastState * 10 - 180, goNavio.transform.localRotation.eulerAngles.z));
+            goNavio.transform.parent.rotation = Quaternion.Euler(Vector3.forward * actualAngulation/2 * lastState + goNavio.transform.parent.rotation.eulerAngles);
+            goNavio.transform.localRotation = Quaternion.Euler(new Vector3(0, lastState *10 * actualAngulation  -  180, goNavio.transform.localRotation.eulerAngles.z));
+           
+            Quaternion rot = goNavio.transform.parent.rotation;
+            goNavio.transform.parent.rotation = new Quaternion(rot.x,rot.y,Mathf.Clamp(rot.z,-0.38f,0.38f),rot.w);
+           
+            deltaAngleX =Mathf.Clamp(Mathf.Abs(goNavio.transform.parent.rotation.z - lastAngle.z)* 300,0,2);
+            lastAngle = goNavio.transform.parent.rotation;
+
+            actualAngulation = (0.137f * lastAngle.z*10 * (-lastAngle.z*10)+ 2);
+
         }
+                
+        
     }
     public void Interact()
     {
