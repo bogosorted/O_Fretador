a5da5e4a157f61c3e98ae0b54ffba1d1f178635a
Gabriel Aragão <gabrielaragaofds@gmail.com>
2021-07-31 20:47:29 -0300

    bug fixed

--- Assets/Scripts/Obj Interagiveis/TimaoBehaviour.cs
+++ Assets/Scripts/Obj Interagiveis/TimaoBehaviour.cs
@@ -22,11 +22,12 @@ public class TimaoBehaviour : MonoBehaviour, Iinteractable
     {
         
         if (controlando)
-            lastState = Mathf.Clamp(lastState + -Input.GetAxisRaw("Horizontal") / 30, -1, 1);
+            lastState = Mathf.Clamp(lastState + -Input.GetAxisRaw("Horizontal")*1f / 30, -1, 1);
         if (!navio.ancorado)
         {
-            goNavio.transform.parent.rotation = Quaternion.Euler(Vector3.forward * actualAngulation/2 * lastState + goNavio.transform.parent.rotation.eulerAngles);
-            goNavio.transform.localRotation = Quaternion.Euler(new Vector3(0, lastState *10 * actualAngulation  -  180, goNavio.transform.localRotation.eulerAngles.z));
+            goNavio.transform.parent.rotation = Quaternion.Euler(Vector3.forward  * lastState + goNavio.transform.parent.rotation.eulerAngles);
+            print(lastState);
+            goNavio.transform.localRotation = Quaternion.Euler(new Vector3(0, lastState*actualAngulation * 10  -  180, goNavio.transform.localRotation.eulerAngles.z));
            
             Quaternion rot = goNavio.transform.parent.rotation;
             goNavio.transform.parent.rotation = new Quaternion(rot.x,rot.y,Mathf.Clamp(rot.z,-0.38f,0.38f),rot.w);
@@ -35,6 +36,8 @@ public class TimaoBehaviour : MonoBehaviour, Iinteractable
             lastAngle = goNavio.transform.parent.rotation;
 
             actualAngulation = (0.137f * lastAngle.z*10 * (-lastAngle.z*10)+ 2);
+            if (actualAngulation < 0.1f)
+                lastState = 0;
 
         }
                 
