c8a472fe32549f0737be90e8475b9e26e0191939
Gabriel Aragão <gabrielaragaofds@gmail.com>
2021-08-01 23:33:45 -0300

    Main Menu

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    [SerializeField]FadeSystemBehaviour fade;
+    
+    public void OnPlay()
+    {
+        StartCoroutine(WaitFade());
+    }
+    IEnumerator WaitFade()
+    {
+        fade.CreateFade(false,1,Random.Range(0,fade.TypesF.Length));
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
